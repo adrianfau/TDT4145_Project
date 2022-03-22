50c46fa9c4418c2044daa6c27232df87483026e0
Kingofrock37 <adrianfauske@gmail.com>
2022-03-22 17:59:10 +0100

    SQL Queries

--- CoffeeDB.py
+++ CoffeeDB.py
@@ -6,17 +6,28 @@ cursor = con.cursor()
 #con.close()
 
 while True:
-    initInput = int(input("Choose function:"))
+    initInput = int(input("Choose function:;"))
 
 
 def postNote(roastery, coffeeName, roastedDate, points, notes):
 
+
 def printUsers():
+    cursor.execute("SELECT User.UserID, User.FullName, COUNT(Post.UserID)"
+                    "WHERE Post.UserID = User.UserID" 
+                    "GROUP BY User.UserID" 
+                    "ORDER BY COUNT(Post.UserID) DESC;")
 
 def valueForMoney():
+    cursor.execute("SELECT Coffee.CoffeeID, Coffee.CoffeeName, Coffee.Price, Post.CoffeeID, Post.Points"
+                    ""
+                    ";")
 
 def searchKeyword(keyword):
+    cursor.execute(f"SELECT Coffee.CoffeeNames, Coffee.Roastery"
+                    "FROM Post INNER JOIN Coffee ON Post.CoffeeID = Coffee.CoffeeID" 
+                    "WHERE Post.Notes CONTAINS {keyword} OR Coffee.Description CONTAINS {keyword}';")
 
 def excludeKeyword(keyword):
-
+    cursor.execute()
 
