b216146091a5ff2d5a6692de81192c68dd8a46fa
Kingofrock37 <adrianfauske@gmail.com>
2022-03-22 19:30:18 +0100

    Expand SQL Queries and Implement Text UI

--- CoffeeDB.py
+++ CoffeeDB.py
@@ -1,33 +1,71 @@
+print("banana")
 import sqlite3
 con = sqlite3.connect("CoffeeDB.db")
 
 cursor = con.cursor()
-#cursor.execute("SELECT * FROM sqlite_master")
-#con.close()
+cursor.execute("SELECT * FROM sqlite_master")
+con.close()
 
-while True:
-    initInput = int(input("Choose function:;"))
+def postNote():
+    print("Post New Note to CoffeeDB")
+    roastery = input("Roastery: ")
+    coffeeName = input("Coffee Name: ")
+    points = input("Points Score: ")
+    notes = input("Notes: ")
+
+    print(f"New Post posted!\n\n")
 
 
-def postNote(roastery, coffeeName, roastedDate, points, notes):
-
-
-def printUsers():
-    cursor.execute("SELECT User.UserID, User.FullName, COUNT(Post.UserID)"
-                    "WHERE Post.UserID = User.UserID" 
+def printTopUsers():
+    print("Printing Top Coffee Drinkers This Year")
+    for row in cursor.execute("SELECT User.UserID, User.FullName, COUNT(Post.UserID)"
+                    "WHERE Post.UserID = User.UserID AND Post.TastingDate = '2022'" 
                     "GROUP BY User.UserID" 
-                    "ORDER BY COUNT(Post.UserID) DESC;")
+                    "ORDER BY COUNT(Post.UserID) DESC"):
+                    print(row)
+
 
 def valueForMoney():
-    cursor.execute("SELECT Coffee.CoffeeID, Coffee.CoffeeName, Coffee.Price, Post.CoffeeID, Post.Points"
+    print("Printing Highest Average Score to Price Ratio")
+    for row in cursor.execute("SELECT Coffee.CoffeeID, Coffee.CoffeeName, Coffee.Price, Post.CoffeeID, Post.Points"
                     ""
-                    ";")
+                    ""):
+                    print(row)
 
-def searchKeyword(keyword):
-    cursor.execute(f"SELECT Coffee.CoffeeNames, Coffee.Roastery"
+
+def searchKeyword():
+    keyword = input("\nEnter a keyword to search for: ")
+    for row in cursor.execute("SELECT Coffee.CoffeeNames, Coffee.Roastery"
                     "FROM Post INNER JOIN Coffee ON Post.CoffeeID = Coffee.CoffeeID" 
-                    "WHERE Post.Notes CONTAINS {keyword} OR Coffee.Description CONTAINS {keyword}';")
+                    "WHERE CONTAINS(Post.Notes, keyword=:keyword) OR CONTAINS(Coffee.Description, keyword=:keyword)", {"keyword": lower(keyword)}):
+                    print(row)
+        
 
-def excludeKeyword(keyword):
-    cursor.execute()
+def includeCountriesExcludeMethod():
+    countries = input("Enter up to three countries to search for: ")
+    method = input("Enter a method to exclude.")
+    for row in cursor.execute("SELECT"):
+        print("row")
 
+functionList = [postNote, printTopUsers, valueForMoney, searchKeyword, includeCountriesExcludeMethod]
+
+while True:
+    print("""Functions:
+    1 | Post Note
+    2 | Print Top Coffee Tasters This Year
+    3 | Print Highest Average Score to Price Ratio
+    4 | Search for Keyword
+    5 | Search For Countries and Exclude Methods
+    6 | Exit Program\n""")
+    try:
+        initInput = int(input("Input Desired Function Number: "))
+    except ValueError:
+        print("Input an integer. ")
+        continue
+    if initInput == 6:
+        print("Exiting Program.\n\n")
+        break
+    elif not 1 <= initInput <= (len(functionList) + 1) :
+        print("Not a function! Try again.\n\n")
+    else:
+        functionList[initInput - 1]()
