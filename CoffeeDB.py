284aa8fd0e27d783de71e391150053d3879839d0
Kingofrock37 <adrianfauske@gmail.com>
2022-03-22 17:10:36 +0100

    Change buildDB to CoffeDB.py

---
+++ CoffeeDB.py
@@ -0,0 +1,22 @@
+import sqlite3
+con = sqlite3.connect("CoffeeDB.db")
+
+cursor = con.cursor()
+#cursor.execute("SELECT * FROM sqlite_master")
+#con.close()
+
+while True:
+    initInput = int(input("Choose function:"))
+
+
+def postNote(roastery, coffeeName, roastedDate, points, notes):
+
+def printUsers():
+
+def valueForMoney():
+
+def searchKeyword(keyword):
+
+def excludeKeyword(keyword):
+
+
--- buildDB.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-con = sqlite3.connect("CoffeeDB.db")
-
-cursor = con.cursor()
-cursor.execute("SELECT * FROM sqlite_master")
-con.close()
