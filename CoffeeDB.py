cf967bbc85fcf39cff425776533db0f3bb3e0874
Kingofrock37 <adrianfauske@gmail.com>
2022-03-25 21:26:03 +0100

    Fix login, queries for top drinkers and searh keyword

--- CoffeeDB.py
+++ CoffeeDB.py
@@ -1,12 +1,9 @@
 import sqlite3
 con = sqlite3.connect("CoffeeDB.db")
 
-cursor = con.cursor()
-cursor.execute("SELECT * FROM sqlite_master")
-con.close()
-
 #To add data to database: Add Farm, ProcessingMethod, User. Then add Bean, add Batch, add BeanInBatch. Then add Coffee, then add Post
 # Example users:
+# test | test | Test Testesen
 # adrianfauske@gmail.com | banan | Adrian Nasir Fauske
 # williarh@stud.ntnu.no | eple | William Strand Hassel
 # kraemer@ntnu.no | 123 | Frank Alexander Kraemer
@@ -21,78 +18,74 @@ def postNote():
     else:
         print("asdasd")
     notes = input("Notes: ")
-
     print(f"New Post posted!\n\n")
 
 def printTopUsers():
-    print("Printing Top Coffee Drinkers This Year")
-    for row in cursor.execute("SELECT U.UserID, U.FullName, COUNT(P.UserID) AS CoffeesDrunk FROM User AS U LEFT JOIN Post AS P"
-                    "WHERE P.UserID = U.UserID AND P.tastingdate LIKE '%2022%'"
-                    "GROUP BY U.UserID"
-                    "ORDER BY COUNT(P.UserID) DESC"):
+    print("\n\nPrinting Top Coffee Drinkers This Year\nName | Cups of coffee drunk")
+    cursor = con.cursor()
+    for row in cursor.execute("SELECT U.FullName, COUNT(P.UserID) AS CoffeesDrunk FROM User AS U LEFT JOIN Post AS P WHERE P.UserID = U.UserID AND P.tastingdate LIKE '%2022%' GROUP BY U.UserID ORDER BY COUNT(P.UserID) DESC"):
                     print(row)
-
 def valueForMoney():
     print("Printing Highest Average Score to Price Ratio")
-    for row in cursor.execute("SELECT C.name, C.priceperkilo, C.roastery, AVG(P.points)/C.priceperkilo AS AvgScoreToPriceRatio"
-                                "FROM Coffee AS C LEFT JOIN Post AS P"
-                                "WHERE C.CoffeeID = P.CoffeeID ORDER BY AvgScoreToPriceRatio DESC"):
+    cursor = con.cursor()
+    for row in cursor.execute("SELECT C.name, C.priceperkilo, C.roastery, AVG(P.points)/C.priceperkilo AS AvgScoreToPriceRatio FROM Coffee AS C LEFT JOIN Post AS P WHERE C.CoffeeID = P.CoffeeID ORDER BY AvgScoreToPriceRatio DESC"):
                     print(row)
 
 def searchKeyword():
-    keyword = input("\nEnter a keyword to search for: ").lower()
-    for row in cursor.execute("Select C.name, C.roastery"
-                                "FROM Coffee AS C LEFT JOIN Post AS P ON P.CoffeeID = C.CoffeeID"
-                                "WHERE P.tastingnotes LIKE keyword=:keyword OR C.description LIKE keyword=:keyword", {"keyword": keyword}):
+    keyword = input("Enter a keyword to search for: ").lower()
+    cursor = con.cursor()
+    print(f"\nPrinting coffees containing keyword {keyword}: ")
+    for row in cursor.execute("SELECT C.name, C.roastery FROM Coffee AS C LEFT JOIN Post AS P ON P.CoffeeID = C.CoffeeID WHERE P.tastingnotes LIKE ? OR C.description LIKE ?", ['%'+keyword+'%', '%'+keyword+'%']):
                     print(row)
         
 def includeCountriesExcludeMethod():
     print("List of available countries: ")
+    cursor = con.cursor()
     for row in cursor.execute("SELECT country FROM Farm"):
         print(row)
     countryInput = (input("Enter up to three countries to search for, separated by a space: ").lower()).split()
-    countries = [countryInput.append(None) for i in range(3-len(countryInput))]
+    countries = countryInput
+    for i in range (3 - len(countryInput)):
+        countries.append(None)
+    #countries.append(None) = countryInput.append(None) for i in range(3-len(countryInput))
 
     print("\nList of all available processing methods: ")
+    cursor = con.cursor()
     for row in cursor.execute("SELECT name from ProcessingMethod"):
         print(row)
-    method = input("Enter a method to exclude.").lower()
+    method = input("Enter a method to exclude: ").lower()
+    print(countries)
 
     print(f"Searching for coffees that are not {method} from countries {countries}.")
-    for row in cursor.execute("SELECT C.name, C.roastery FROM Coffee AS C"
-                                "INNER JOIN Batch ON Coffee.BatchID = Batch.BatchID"
-                                "INNER JOIN ProcessingMethod AS P on B.ProcessingMethodID = P.ProcessingMethodID"
-                                "INNER JOIN Farm AS F WHERE B.FarmID = F.FarmID" 
-                                "AND (method=:method IS NULL OR P.name IS NOT method=:method"
-                                "AND (country1=:country1 IS NULL OR LOWER(F.country) = country1=:country1"
-                                "AND (country2=:country2 IS NULL OR LOWER(F.country) = country2=:country2"
-                                "AND (country3=:country3 IS NULL OR LOWER(F.country) = country3=:country3", {"method": method, "country1": countries[0], "country2": countries[1], "country3": countries[3]}):
+    cursor = con.cursor()
+    for row in cursor.execute("SELECT C.name, C.roastery FROM Coffee AS C INNER JOIN Batch AS B ON Coffee.BatchID = B.BatchID INNER JOIN ProcessingMethod AS P on B.ProcessingMethodID = P.ProcessingMethodID INNER JOIN Farm AS F WHERE B.FarmID = F.FarmID AND (P.name=:method IS NULL OR P.name IS NOT =:method) AND (F.country=:country1 IS NULL OR LOWER(F.country) =:country1) AND (F.country=:country2 IS NULL OR LOWER(F.country) =:country2) AND (F.country=:country3 IS NULL OR LOWER(F.country) =:country3)", {"method": method, "country1": countries[0], "country2": countries[1], "country3": countries[2]}):
                     print(row)
 
 while True: #Login check
     print("Welcome to CoffeeDB.")
     email = input("Please enter email: ")
     password = input("Please enter password: ")
-    try:
-        login = cursor.execute("SELECT User.email, User.password"
-                                "WHERE User.email IS email=:email"
-                                "AND User.password IS password=:password", {"email": email, "password": password})
-    except ValueError:
-        print("Could not find user. Try again: ")
+    cursor = con.cursor()
+    login = cursor.execute("SELECT email, password FROM User WHERE email =:email AND password =:password", {"email": email, "password": password})
+    if login.fetchone() != None:
+        cursor = con.cursor()
+        name = cursor.execute("SELECT fullname FROM User WHERE email=:email", {"email": email})
+        print(f"Logged in as {name.fetchone()}")
+        break
+    else:
+        print("Error logging in. Try again.\n")
         continue
-    name = cursor.execute("SELECT name FROM User WHERE email=:email", {"email": email})
-    print(f"Logged in as {name}\n")
 
 functionList = [postNote, printTopUsers, valueForMoney, searchKeyword, includeCountriesExcludeMethod]
 
 while True: #Main program loop
-    print("""Functions:
+    print("""\n\nFunctions:
     1 | Post Note
     2 | Print Top Coffee Tasters This Year
     3 | Print Highest Average Score to Price Ratio
     4 | Search for Keyword
     5 | Search For Countries and Exclude Methods
-    6 | Exit Program\n""")
+    6 | Exit Program""")
     try:
         initInput = int(input("Input Desired Function Number: "))
     except ValueError:
@@ -105,3 +98,5 @@ while True: #Main program loop
         print("Not a function! Try again.\n\n")
     else:
         functionList[initInput - 1]()
+
+con.close()
