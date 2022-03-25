717eb4ea2bb3b5a5825575d8ca879fe380470a4e
Kingofrock37 <adrianfauske@gmail.com>
2022-03-25 22:58:26 +0100

    fix keyword, postnotes

--- CoffeeDB.py
+++ CoffeeDB.py
@@ -9,33 +9,40 @@ con = sqlite3.connect("CoffeeDB.db")
 # kraemer@ntnu.no | 123 | Frank Alexander Kraemer
 
 def postNote():
-    print("Post New Note to CoffeeDB:")
-    roastery = input("Roastery: ")
-    coffeeName = input("Coffee Name: ")
+    print("\nPost New Note to CoffeeDB:")
+    cursor = con.cursor()
+    myID = int(cursor.execute("SELECT UserID FROM User WHERE email=:email", {"email": email}).fetchone()[0])
+    #myID = cursor.fetchone()
+    #myID = int(myID[0])
+
+    print("List of roasteries: ")
+    for row in cursor.execute("SELECT DISTINCT roastery FROM Coffee"):
+        print(row)
+    roastery = input("Select which roastery your coffee is from: ")
+
+    print("List of coffees from selected roastery: ")
+    for row in cursor.execute("SELECT name FROM Coffee WHERE roastery=:roastery", {"roastery": roastery}):
+        print(row)
+    coffeeName = input("Select which coffee you have drunk: ")
+    coffeeID = cursor.execute("SELECT CoffeeID FROM Coffee WHERE name=:coffeeName", {"coffeeName": coffeeName})
+
     while True:
         points = int(input("Points Score: "))
         if not 0 <= points <= 10:
             print("Please enter a valid score. ")
         break
+    notes = input("Write your tasting notes about your coffee: ")
 
-    notes = input("Notes: ")
-    cursor = con.cursor()
-    print("List of roasteries: ")
-    for row in cursor.execute("SELECT DISTINCT roastery FROM Coffee"):
-        print(row)
-    inRoastery = input("Select which roastery your coffee is from.")
-    roasteryTest = cursor.execute("SELECT Roastery FROM Coffee INNER JOIN Post ON Coffee.CoffeeID = Post.coffeeID")
+    #roasteryTest = cursor.execute("SELECT Roastery FROM Coffee INNER JOIN Post ON Coffee.CoffeeID = Post.coffeeID")
     # cursor.execute("INSERT INTO Post (tastingnotes, points) VALUES ({tastingnotes}, {points})")
     # val = (tastingnotes, points)
     # cursor.execute(sql, val)
-    cursor.execute("SELECT UserID FROM User WHERE email=:email", {"email": email})
-    myID = cursor.fetchone()
-    myID = int(myID[0])
+
     cursor.execute("SELECT CoffeeID FROM Coffee WHERE name=:name", {"name": coffeeName})
     coffeeID = cursor.fetchone()
     coffeeID = int(coffeeID[0])
 
-    cursor.execute("INSERT INTO Post (tastingnotes, tastingdate, UserID, CoffeeID, points) VALUES (?, CURRENT_DATE, ?, ?, ?)", (notes, myID, 1, points,))
+    cursor.execute("INSERT INTO Post (tastingnotes, tastingdate, UserID, CoffeeID, points) VALUES (?, CURRENT_DATE, ?, ?, ?)", (notes, myID, coffeeID, points,))
 
     con.commit()
     print(f"\nNew Post posted!")
@@ -43,11 +50,13 @@ def postNote():
 def printTopUsers():
     print("\n\nPrinting Top Coffee Drinkers This Year\nName | Cups of coffee drunk")
     cursor = con.cursor()
+    print("Full Name | Coffee Count")
     for row in cursor.execute("SELECT U.FullName, COUNT(P.UserID) AS CoffeesDrunk FROM User AS U LEFT JOIN Post AS P WHERE P.UserID = U.UserID AND P.tastingdate LIKE '%2022%' GROUP BY U.UserID ORDER BY COUNT(P.UserID) DESC"):
                     print(row)
 def valueForMoney():
-    print("Printing Highest Average Score to Price Ratio")
+    print("\nPrinting Highest Average Score to Price Ratio")
     cursor = con.cursor()
+    print("Coffee Name | Price Per Kilo ,- | Roastery | Average Score To Price Ratio")
     for row in cursor.execute("SELECT C.name, C.priceperkilo, C.roastery, AVG(P.points)/C.priceperkilo AS AvgScoreToPriceRatio FROM Coffee AS C LEFT JOIN Post AS P WHERE C.CoffeeID = P.CoffeeID ORDER BY AvgScoreToPriceRatio DESC"):
                     print(row)
 
@@ -55,7 +64,8 @@ def searchKeyword():
     keyword = input("Enter a keyword to search for: ").lower()
     cursor = con.cursor()
     print(f"\nPrinting coffees containing keyword {keyword}: ")
-    for row in cursor.execute("SELECT C.name, C.roastery FROM Coffee AS C LEFT JOIN Post AS P ON P.CoffeeID = C.CoffeeID WHERE P.tastingnotes LIKE ? OR C.description LIKE ?", ['%'+keyword+'%', '%'+keyword+'%']):
+    print("Coffee Name | Roastery")
+    for row in cursor.execute("SELECT C.name, C.roastery FROM Coffee AS C LEFT JOIN Post AS P ON P.CoffeeID = C.CoffeeID WHERE P.tastingnotes LIKE ? OR C.description LIKE ? GROUP BY C.CoffeeID", ['%'+keyword+'%', '%'+keyword+'%']):
                     print(row)
         
 def includeCountriesExcludeMethod():
@@ -77,10 +87,10 @@ def includeCountriesExcludeMethod():
     for row in cursor.execute("SELECT name from ProcessingMethod"):
         print(row)
     method = input("Enter a method to exclude: ").lower()
-    print(countries)
 
     print(f"\nSearching for coffees that are not {method} from countries {countries}.")
     cursor = con.cursor()
+    print("Coffee Name | Roastery")
     for row in cursor.execute("SELECT C.name, C.roastery FROM Coffee AS C INNER JOIN Batch AS B ON C.BatchID = B.BatchID INNER JOIN ProcessingMethod AS P on B.ProcessingMethodID = P.ProcessingMethodID INNER JOIN Farm AS F WHERE B.FarmID = F.FarmID AND P.name IS NOT :method AND ((:country1 IS NULL OR LOWER(F.country) =:country1) OR (:country2 IS NULL OR LOWER(F.country) =:country2))", {"method": method, "country1": countries[0], "country2": countries[1]}):
                     print(row)
 
